07102dbfe48a97218beaf54ac9906719c48e4660
samirammar <samirammar.officially@gmail.com>
2024-01-07 00:45:13 +0400

    MODIFY -> change auth layout from h-full to h-screen

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react"
 
 const AuthLayout = ({children}:{children: React.ReactNode}) => {
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex items-center justify-center h-screen">
         {children}
     </div>
   )
