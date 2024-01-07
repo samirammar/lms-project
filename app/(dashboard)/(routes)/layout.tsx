dd1b66eefe8b68d6aac3f4af837631a61a738498
samirammar <samirammar.officially@gmail.com>
2024-01-08 01:44:54 +0400

    ADD -> navbar component to dashboard

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -1,13 +1,16 @@
 import React from "react"
 import Sidebar from "../_components/sidebar"
+import Navbar from "../_components/navbar"
 
 
 const DashboardLayout = ({children}:{children: React.ReactNode}) => {
   return (
     <div className="h-full">
-       
+        <div className="h-[80px] inset-y-0 md:pr-56 w-full fixed z-50">
+          <Navbar />
+        </div>
         <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
-            <Sidebar />
+          <Sidebar />
         </div>
         <main className="md:pr-56 h-full">
           {children}
